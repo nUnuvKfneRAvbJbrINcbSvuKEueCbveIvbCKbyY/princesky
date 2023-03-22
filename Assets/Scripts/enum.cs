9e4f791448fb0b4ba5a08e8f29472345a0132fb2
nUnuvKfneRAvbJbrINcbSvuKEueCbveIvbCKbyY <104197913+nUnuvKfneRAvbJbrINcbSvuKEueCbveIvbCKbyY@users.noreply.github.com>
2023-03-22 19:37:19 +0100

    utfzv

--- Assets/Scripts/enum.cs
+++ Assets/Scripts/enum.cs
@@ -2,30 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enum : MonoBehaviour
+public class MyEnum : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum Saty
     {
-        public enum saty
-{
-    s1,
-    s2,
-    s3,
-    s4,
-    s5,
-    s6,
-    s7,
-    s8,
-    s9,
-    s10,
-    s11,
-}
+        vm1,
+        vm2,
+        vm3,
+        s1,
+        s2,
+        s3,
+        s4,
+        s5,
+        s6,
+        s7,
+        s8,
+        s9,
+        s10,
+        s11,
+        t1,
+        t2,
+        t3,
+        t4,
+        d1,
+        d2,
+        d3,
+        d4,
+        d5,
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+
+    }
+
     void Update()
     {
-        
+
     }
 }
